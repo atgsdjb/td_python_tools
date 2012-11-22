fe525b18c307dd10303d4bf5c60cd7fbaafab5a6
dongjb <td.seraphim@gmail.com>
2012-11-22 21:24:32 +0800

    add base.py for study

---
+++ PythonThink/src/serapt/td/__init__.py
@@ -0,0 +1,6 @@
+'''
+'''
+if __name__ == '__main__':
+    st = 'seraphim'
+    us = u'seraphim'
+    print type(st),type(us)
---
+++ PythonThink/src/serapt/td/study/base.py
@@ -0,0 +1,6 @@
+'''
+'''
+if __name__ == '__main__':
+    f =open('test.txt','r')
+    s = f.read()
+    print s,type(s)
