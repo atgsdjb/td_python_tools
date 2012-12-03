d0be971c81edcda3c6ef274ce9f4f9b72c4bb07c
= <=>
2012-12-03 21:41:39 +0800

    test ubuntu git hub

--- PythonThink/src/td_re.py
+++ PythonThink/src/td_re.py
@@ -6,4 +6,7 @@ print(m.group())
 m2 = re.match("0(x|X)[1-9a-fA-F][0-9a-fA-F]+",'0x122202345f')
 print(m2.group())
 str = os.popen('dir','r')
+l =[i for range(12)]
+print l
+print type(l)
 print(str)
